b42980cbf8b83a029329ed2294d282bd16bc067b
ishidamu <ishidamu@users.noreply.github.com>
2019-10-17 10:24:35 +0900

    不具合修正

--- src/test/resources/jp/ossc/nimbus/plugin/1/1_1/answer.java
+++ src/test/resources/jp/ossc/nimbus/plugin/1/1_1/answer.java
@@ -6,31 +6,31 @@ TEST =
  11 
 
 TEST >
- 6 
- 7 
- 8 
- 9 
+/* **Java Version Difference Comment Start** 6 **Java Version Difference Comment End** */
+/* **Java Version Difference Comment Start** 7 **Java Version Difference Comment End** */
+/* **Java Version Difference Comment Start** 8 **Java Version Difference Comment End** */
+/* **Java Version Difference Comment Start** 9 **Java Version Difference Comment End** */
 /* **Java Version Difference Comment Start** 11 **Java Version Difference Comment End** */
 
 TEST >=
+/* **Java Version Difference Comment Start** 6 **Java Version Difference Comment End** */
+/* **Java Version Difference Comment Start** 7 **Java Version Difference Comment End** */
+/* **Java Version Difference Comment Start** 8 **Java Version Difference Comment End** */
+/* **Java Version Difference Comment Start** 9 **Java Version Difference Comment End** */
+ 11 
+
+TEST <
  6 
  7 
  8 
  9 
- 11 
-
-TEST <
-/* **Java Version Difference Comment Start** 6 **Java Version Difference Comment End** */
-/* **Java Version Difference Comment Start** 7 **Java Version Difference Comment End** */
-/* **Java Version Difference Comment Start** 8 **Java Version Difference Comment End** */
-/* **Java Version Difference Comment Start** 9 **Java Version Difference Comment End** */
 /* **Java Version Difference Comment Start** 11 **Java Version Difference Comment End** */
 
 TEST <=
-/* **Java Version Difference Comment Start** 6 **Java Version Difference Comment End** */
-/* **Java Version Difference Comment Start** 7 **Java Version Difference Comment End** */
-/* **Java Version Difference Comment Start** 8 **Java Version Difference Comment End** */
-/* **Java Version Difference Comment Start** 9 **Java Version Difference Comment End** */
+ 6 
+ 7 
+ 8 
+ 9 
  11 
 
 てすと
